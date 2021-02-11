a509065cbc5a9ffdcc066fbdc6e1c4d5714c1d3d
Guilherme de Agostini da Silva <guilher.agostini@gmail.com>
2021-02-11 17:08:36 -0300

    Projeto

---
+++ Loja/ASP.Net/ICatalogo.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace LojaLivro
+{
+    public interface ICatalogo
+    {
+        List<Livro> GetLivro();
+    }
+}
---
+++ Loja/ASP.Net/Relatorio.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace LojaLivro
+{
+    public class Relatorio : IRelatorio
+    {
+        private readonly ICatalogo catalogo;
+
+        public Relatorio(ICatalogo catalogo)
+        {
+            this.catalogo = catalogo;
+        }
+
+        public async Task Imprimir(HttpContext context)
+        {
+            foreach (var livro in catalogo.GetLivro())
+            {
+                await context.Response.WriteAsync($"{livro.Codigo}  {livro.Titulo}  R${livro.Preco} \n");
+            }
+        }
+    }
+}
