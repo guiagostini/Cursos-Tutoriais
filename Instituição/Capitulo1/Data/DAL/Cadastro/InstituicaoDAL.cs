94e13d4089e2ccef92d328daacd027421690e788
Guilherme de Agostini da Silva <guilher.agostini@gmail.com>
2021-02-16 09:47:50 -0300

    Projeto

---
+++ Instituição/Capitulo1/Data/DAL/Cadastro/InstituicaoDAL.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Modelo.Cadastros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capitulo1.Data.DAL.Cadastro
+{
+    public class InstituicaoDAL
+    {
+        private IESContext _context;
+
+        public InstituicaoDAL(IESContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Instituicao> ObterInstituicoesClassificadasPorNome()
+        {
+            return _context.Instituicao.OrderBy(b => b.Nome);
+        }
+
+        public async Task<Instituicao> ObterInstituicaoPorID(long id)
+        {
+            return await _context.Instituicao.Include(d => d.Departamentos).SingleOrDefaultAsync(m => m.InstituicaoID == id);
+        }
+
+        public async Task<Instituicao> GravarInstituicao(Instituicao instituicao)
+        {
+            if (instituicao.InstituicaoID == null)
+            {
+                _context.Instituicao.Add(instituicao);
+            }
+            else
+            {
+                _context.Update(instituicao);
+            }
+
+            await _context.SaveChangesAsync();
+            return instituicao;
+        }
+
+        public async Task<Instituicao> EliminarInstituicaoPorID(long id)
+        {
+            Instituicao instituicao = await ObterInstituicaoPorID(id);
+            _context.Instituicao.Remove(instituicao);
+            await _context.SaveChangesAsync();
+            return instituicao;
+        }
+    }
+}
---
+++ Instituição/Capitulo1/Data/IESContext.cs
@@ -0,0 +1,60 @@
+﻿using Modelo.Cadastros;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Modelo.Discente;
+using Capitulo1.Models.Infra;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Modelo.Docente;
+
+namespace Capitulo1.Data
+{
+    public class IESContext : IdentityDbContext<UsuarioDaAplicacao>
+    { 
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Instituicao> Instituicao { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Disciplina> Disciplinas { get; set; }
+        public DbSet<Academico> Academicos { get; set; }
+        public DbSet<Professor> Professores { get; set; }
+
+
+        public IESContext(DbContextOptions<IESContext> options) 
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<CursoDisciplina>()
+                .HasKey(pk => new { pk.CursoID, pk.DisciplinaID });
+
+            modelBuilder.Entity<CursoDisciplina>()
+                .HasOne(c => c.Curso)
+                .WithMany(cd => cd.CursoDisciplinas)
+                .HasForeignKey(fk => fk.CursoID);
+
+            modelBuilder.Entity<CursoDisciplina>()
+                .HasOne(d => d.Disciplina)
+                .WithMany(cd => cd.CursoDisciplinas)
+                .HasForeignKey(fk => fk.DisciplinaID);
+
+            modelBuilder.Entity<CursoProfessor>()
+                .HasKey(cd => new { cd.CursoID, cd.ProfessorID });
+
+            modelBuilder.Entity<CursoProfessor>()
+                .HasOne(c => c.Curso)
+                .WithMany(cd => cd.CursosProfessores)
+                .HasForeignKey(c => c.CursoID);
+
+            modelBuilder.Entity<CursoProfessor>()
+                .HasOne(d => d.Professor)
+                .WithMany(cd => cd.CursosProfessores)
+                .HasForeignKey(d => d.ProfessorID);
+        }
+    }
+}
